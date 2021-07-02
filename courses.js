a62a69e5db337d69da3e61bc633672caa0fcf250
Stefan Her <stefan.her@gmail.com>
2021-07-02 15:53:54 +0200

    course

---
+++ courses.js
@@ -0,0 +1,96 @@
+"use strict";
+
+const formNode = document.getElementsByTagName("form")[0];
+const inputItem = formNode.elements[0];
+const inputPrice = formNode.elements[1];
+const budgetSpan = document.getElementById("display").getElementsByTagName("span")[0];
+const maxSpendSpan = document.getElementById("display").getElementsByTagName("span")[1];
+const middleSpendSpan = document.getElementById("display").getElementsByTagName("span")[2];
+
+const items = new Set();
+let maxSpend = NaN;
+let middleSpend = NaN;
+
+init();
+
+function init() {
+    if(isNaN(maxSpend)) {
+        maxSpend = parseInt(prompt("insert max Budget : "));
+        if(!isNaN(maxSpend)) { updateSpan(budgetSpan, maxSpend); }
+        else { init(); }
+        
+    }
+}
+
+function updateSpan(span, v) {
+    span.innerHTML = v;
+}
+
+function eventSubmit(e) { 
+    e.preventDefault();
+    addToListe();
+    searchMax();
+    middle();
+}
+
+function middle() {
+    let a = [];
+    let total = 0;
+    items.forEach(function(value) { 
+        total += value.get("price"); 
+    });
+
+    console.log(total +'---'+ items.size);
+    let middle = total / items.size;
+    updateSpan(middleSpendSpan, middle);
+}   
+
+function searchMax() {
+    let max = 0;
+    items.forEach(function(value) {
+        if(value.get("price") > max) { 
+            max = value.get("price");
+        }
+    });
+    updateSpan(maxSpendSpan, max);
+}
+
+function addToListe() {
+    let element = inputItem.value;
+    let price = parseInt(inputPrice.value);
+
+    if(element != "" && !isNaN(price)) {
+
+        if((maxSpend - price) > 0) {
+
+            let item = new Map();
+            item.set("item", element);
+            item.set("price", price);
+
+            let flag = true;
+            items.forEach(function(value) {
+                if(value.get("price") == price) {
+                    let conf = confirm(`le prix ${value.get("price")} pour  ${value.get("item")} existe déjà`);
+                    if(!conf) { flag = false; }
+                }
+            });
+
+            if(flag) {
+                items.add(item);
+                maxSpend -= price;
+                updateSpan(budgetSpan, maxSpend);
+            }            
+        } else {
+            alert("plus de budget");
+        }
+
+    }
+}
+
+formNode.addEventListener("submit", eventSubmit);
+
+
+
+
+
+
